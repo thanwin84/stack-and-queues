705ea33ef372a0ee1f457baaa46d68820fecc3b5
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-08-07 22:19:58 +0600

    Update trapping rain water.cpp

--- stacks/trapping rain water.cpp
+++ stacks/trapping rain water.cpp
@@ -1,3 +1,4 @@
+//time: O(n) and  space: O(n)
 class Solution {
 public:
     int trap(vector<int>& height) {
@@ -23,4 +24,33 @@ public:
         }
         return trapped_water;
     } 
+};
+//time: O(n) and space: O(1)
+//two pointer approach
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if (height.size() < 3){
+            return 0;
+        }
+        int maxL = height[0];
+        int maxR = height[height.size()-1];
+        int left = 1; // we can't trap water in the first building
+        int right = height.size() -1; // we can't trap water in the last building
+        int trapped_water = 0;
+        while (left <= right){
+            maxL = max(maxL, height[left]);
+            maxR = max(maxR, height[right]);
+            //fill water from lowest side
+            if (maxL < maxR){
+                trapped_water += maxL - height[left];
+                left++;
+            }
+            else {
+                trapped_water += maxR - height[right];
+                right--;
+            }
+        }
+        return trapped_water;
+    }
 };
