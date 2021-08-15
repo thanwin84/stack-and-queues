c470203bb4fcfd540375aa0d05fe8390bbd2a93f
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-08-15 17:54:40 +0600

    Create queue using two stack.cpp

---
+++ queue using two stack.cpp
@@ -0,0 +1,53 @@
+//push: O(1) ans pop: Amortized O(1)
+class MyQueue {
+public:
+    /** Initialize your data structure here. */
+    vector<int> stack1;
+    vector<int> stack2;
+    MyQueue() {
+
+    }
+
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        stack1.push_back(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        int item;
+        if (stack2.size() == 0) {
+            while (stack1.size() > 0) {
+                stack2.push_back(stack1.back());
+                stack1.pop_back();
+            }
+            item = stack2.back();
+            stack2.pop_back();
+        }
+        else {
+            item = stack2.back();
+            stack2.pop_back();
+        }
+        return item;
+    }
+
+    /** Get the front element. */
+    int peek() {
+        int item;
+        if (stack2.size() == 0) {
+            while (stack1.size() > 0) {
+                stack2.push_back(stack1.back());
+                stack1.pop_back();
+            }
+            item = stack2.back();
+            
+        }
+        item = stack2.back();
+        return item;
+    }
+
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return stack1.size() == 0 and stack2.size() == 0;
+    }
+};
