2c86dcd5049bf45c78677ab51053831233cbecde
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-01-17 15:01:42 +0600

    Update lru.cpp

--- lru.cpp
+++ lru.cpp
@@ -28,37 +28,28 @@ public:
     int get(int key) {
         if (table.count(key)) {
             Node* temp = table[key];
-            Node* before_temp = temp->prev;
-            Node* after_temp = temp->next;
-            before_temp->next = after_temp;
-            after_temp->prev = before_temp;
             int result = temp->data;
-            //adding current used node at the front
-            Node* after_head = head->next;
-            head->next = temp;
-            temp->prev = head;
-            temp->next = after_head;
-            after_head->prev = temp;
+            delete_node(temp);
+            //adding current node at the front
+            add_node(temp);
             return result;
         }
         else {
             return -1;
         }
     }
-    void add_node(int key, int value) {
-        Node* new_node = new Node(key, value);
-        table[key] = new_node;
+    void add_node(Node * new_node) {
         Node* after_head = head->next;
         new_node->next = after_head;
         new_node->prev = head;
         head->next = new_node;
         after_head->prev = new_node;
     }
-    void delete_node() {
-        table.erase(tail->prev->key);
-        Node* lru = tail->prev;
-        lru->prev->next = tail;
-        tail->prev = lru->prev;
+    void delete_node(Node*del_node) {
+        Node* del_prev = del_node->prev;
+        Node* del_next = del_node->next;
+        del_prev->next = del_next;
+        del_next->prev = del_prev;
     }
     void print() {
         Node* current = head->next;
@@ -67,35 +58,29 @@ public:
             current = current->next;
         }
         cout << endl;
-        
+
 
     }
     void put(int key, int value) {
         if (!table.count(key)) {
             count++;
-            if (count <= capacity) {
-                add_node(key, value);
-            }
-            else {
-                //deleting the last least recent used node
+            if (count > capacity) {
                 count--;
-                delete_node();
-                add_node(key, value);
+                Node* lru = tail->prev;
+                //delete the lru element
+                delete_node(lru);
+                //delete the lru element
+                table.erase(lru->key);
             }
+            Node* new_node = new Node(key, value);
+            table[key] = new_node;
+            add_node(new_node);
         }
         else {
             table[key]->data = value;
-            Node* temp = table[key];
-            Node* before_temp = temp->prev;
-            Node* after_temp = temp->next;
-            before_temp->next = after_temp;
-            after_temp->prev = before_temp;
-            Node* after_head = head->next;
-            temp->next = after_head;
-            temp->prev = head;
-            head->next = temp;
-            after_head->prev = temp;
-
+            Node* move_node = table[key];
+            delete_node(move_node);
+            add_node(move_node);
         }
     }
 };
